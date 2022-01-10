c2af32867bd70832a75b9d38f527393eb0a03c89
xuyanbin <xu_yanbin2005@126.com>
2022-01-10 12:39:00 +0800

    try阶段请求失败抛出异常 避免调用方再去判断返回结果的正确性，可以直接反序列化为自己需要的强类型

--- src/Dtmcli/Tcc/Tcc.cs
+++ src/Dtmcli/Tcc/Tcc.cs
@@ -11,23 +11,22 @@ namespace Dtmcli
 {
     public class Tcc
     {
-        public IdGenerator idGen;
-        public string dtm;
-        private string gid;
-        private IDtmClient dtmClient;
+        public IdGenerator IdGen;
+        private readonly IDtmClient _dtmClient;
 
         public Tcc(IDtmClient dtmHttpClient, string gid)
         {
-            this.dtmClient = dtmHttpClient;
+            this._dtmClient = dtmHttpClient;
             this.Gid = gid;
-            this.idGen = new IdGenerator();
+            this.IdGen = new IdGenerator();
         }
 
-        public string Gid { get => gid; set => gid = value; }
+        public string Gid { get; set; }
 
         public async Task<string> CallBranch(object body, string tryUrl, string confirmUrl, string cancelUrl, CancellationToken cancellationToken = default)
         {
-            var branchId = this.idGen.NewBranchId();
+            var dataStr = JsonSerializer.Serialize(body);
+            var branchId = this.IdGen.NewBranchId();
             var registerTccBranch = new RegisterTccBranch()
             {
                 Branch_id = branchId,
@@ -37,18 +36,21 @@ namespace Dtmcli
                 Trans_type = "tcc",
                 Gid = this.Gid,
                 Try = tryUrl,
-                Data = JsonSerializer.Serialize(body)
+                Data = dataStr
             };
             
-            await dtmClient.RegisterTccBranch(registerTccBranch,cancellationToken);
+            await _dtmClient.RegisterTccBranch(registerTccBranch,cancellationToken);
 
             var tryHttpClient = new HttpClient();
-            var tryContent = new StringContent(JsonSerializer.Serialize(body));
+            var tryContent = new StringContent(dataStr);
             tryContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            tryUrl = $"{tryUrl}?gid={this.Gid}&trans_type=tcc&branch_id={branchId}&branch_type=try";
+            tryUrl = $"{tryUrl}?gid={this.Gid}&trans_type=tcc&branch_id={branchId}&op=try";
  
             var response = await tryHttpClient.PostAsync(tryUrl, tryContent);
-            return await response.Content.ReadAsStringAsync();
+            var responseContent= await response.Content.ReadAsStringAsync(cancellationToken);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($"failed to request during try phase[{tryUrl}],httpStatus[{(int)response.StatusCode}],reasonPhrase[{response.ReasonPhrase}],content[{responseContent}]");
+            return responseContent;
         }
  
     }
