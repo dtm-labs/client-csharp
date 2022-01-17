66d5c46f8b85a50c76747a3d8c1eaa3b0a154a69
catcherwong <catcher_hwq@outlook.com>
2022-01-17 14:36:27 +0800

    fix pr feed back

--- src/Dtmcli/Tcc/TccGlobalTransaction.cs
+++ src/Dtmcli/Tcc/TccGlobalTransaction.cs
@@ -19,26 +19,8 @@ namespace Dtmcli
         public async Task<string> Excecute(Func<Tcc,Task> tcc_cb, CancellationToken cancellationToken =default)
         {
             var gid = await this.GenGid(cancellationToken);
-            var tcc = new Tcc(this.dtmClient, DtmImp.TransBase.NewTransBase(gid, Constant.Request.TYPE_TCC, ""));
-          
-            try
-            {
-                var prepare = await dtmClient.TransCallDtm(tcc.GetTransBase(), tcc.GetTransBase(), Constant.Request.OPERATION_PREPARE, cancellationToken);             
-                logger.LogDebug("prepare result gid={gid}, res={res}", gid, prepare);
 
-                await tcc_cb(tcc);
-
-                var submit = await dtmClient.TransCallDtm(tcc.GetTransBase(), tcc.GetTransBase(), Constant.Request.OPERATION_SUBMIT, cancellationToken);
-                logger.LogDebug("submit result gid={gid}, res={res}", gid, submit);
-            }
-            catch(Exception ex)
-            {
-                logger.LogError(ex,"submitting or abort global transaction error");
-                var abort = await dtmClient.TransCallDtm(tcc.GetTransBase(), tcc.GetTransBase(), Constant.Request.OPERATION_ABORT, cancellationToken);
-                logger.LogDebug("abort result gid={gid}, res={res}", gid, abort);
-                return string.Empty;
-            }
-            return gid;
+            return await Excecute(gid, tcc_cb, cancellationToken);
         }
 
         public async Task<string> Excecute(string gid, Func<Tcc, Task> tcc_cb, CancellationToken cancellationToken = default)
