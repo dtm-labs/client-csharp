f6d0fbe0afec4a27670337a3876f428350a2044f
catcherwong <catcher_hwq@outlook.com>
2022-02-03 13:31:12 +0800

    tests: add ut for tcc

--- src/Dtmcli/ServiceCollectionExtensions.cs
+++ src/Dtmcli/ServiceCollectionExtensions.cs
@@ -5,9 +5,6 @@ namespace Dtmcli
 {
     public static class ServiceCollectionExtensions
     {
-        internal static IServiceCollection ServiceCollection;
-
-
         public static IServiceCollection AddDtmcli(this IServiceCollection services, Action<DtmOptions> setupAction)
         {
             if(setupAction == null)
@@ -16,7 +13,7 @@ namespace Dtmcli
             }
             var options = new DtmOptions();
             setupAction(options);
-            ServiceCollection = services;
+
             services.AddHttpClient("dtmClient", client =>
             {
                 client.BaseAddress = new Uri(options.DtmUrl);
@@ -26,7 +23,6 @@ namespace Dtmcli
 
             services.AddSingleton<TccGlobalTransaction>();
 
-            // 子事务屏障
             services.AddSingleton<IBranchBarrierFactory, DefaultBranchBarrierFactory>();
             return services;
         }
