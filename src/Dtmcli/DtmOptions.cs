7e094b022c902be161bbf690c1c11a8155e1bae4
stulzq <stulzq@qq.com>
2022-02-18 17:48:14 +0800

    add http request timeout option #21

--- src/Dtmcli/DtmOptions.cs
+++ src/Dtmcli/DtmOptions.cs
@@ -1,8 +1,20 @@
-﻿namespace Dtmcli
+﻿using System;
+
+namespace Dtmcli
 {
     public class DtmOptions
     {
         public string DtmUrl { get; set; }
+
+        /// <summary>
+        /// dtm server http request timeout in seconds, default 100
+        /// </summary>
+        public int DtmHttpTimeout { get; set; } = 100;
+
+        /// <summary>
+        /// branch http request timeout in seconds, default 100
+        /// </summary>
+        public int BranchHttpTimeout { get; set; } = 100;
 
         public string DBType { get; set; } = "mysql";
 
