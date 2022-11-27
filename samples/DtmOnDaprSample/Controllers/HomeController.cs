d71da023067b69a4de577e6449c565f920d45c28
Yongguang Zhu <yoz@microsoft.com>
2022-11-27 11:22:48 +0800

    add dapr projects

---
+++ samples/DtmOnDaprSample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DtmSample.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Index()
+        {
+            return Redirect("~/swagger");
+        }
+    }
+}
---
+++ src/DtmDapr/DtmDaprDriver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DtmDapr
+{
+    public static class DtmDaprDriver
+    {
+        public static string GenerateDtmUrl(string dtmAppId = "dtm")
+        {
+            var httpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500";
+
+            return $"http://localhost:{httpPort}/v1.0/invoke/{dtmAppId}/method";
+        }
+
+        public static string AddrForHTTP(string appId, string method)
+        {
+            return $"{Consts.SchemaHTTP}://DAPR_ENV/{appId}/{method}";
+        }
+    }
+}
