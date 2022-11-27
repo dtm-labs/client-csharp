999444509d529f9e79a2ab749643d71442b82493
Yongguang Zhu <yoz@microsoft.com>
2022-11-27 16:02:42 +0800

    change saga using dapr

--- src/DtmDapr/DtmDaprDriver.cs
+++ src/DtmDapr/DtmDaprDriver.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dtmcli;
+using DtmCommon;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Text;
 
 namespace DtmDapr
@@ -16,6 +19,38 @@ namespace DtmDapr
         public static string AddrForHTTP(string appId, string method)
         {
             return $"{Consts.SchemaHTTP}://DAPR_ENV/{appId}/{method}";
+        }
+
+        public static Saga AddUseDapr(this Saga self, string appId, string actionMethod, string compensateMethod, object postData)
+        {
+            return self.Add(AddrForHTTP(appId, actionMethod),
+                string.IsNullOrWhiteSpace(compensateMethod) ? "" : AddrForHTTP(appId, compensateMethod),
+                postData);
+        }
+
+        public static Msg AddUseDapr(this Msg self, string appId, string actionMethod, object postData)
+        {
+            return self.Add(AddrForHTTP(appId, actionMethod), postData);
+        }
+
+        public static Task PrepareUseDapr(this Msg self, string appId, string queryPreparedMethod, CancellationToken cancellationToken = default)
+        {
+            return self.Prepare(AddrForHTTP(appId, queryPreparedMethod), cancellationToken);
+        }
+
+        public static Task DoAndSubmitDbUseDapr(this Msg self, string appId, string queryPreparedMethod, DbConnection db, Func<DbTransaction, Task> busiCall, CancellationToken cancellationToken = default)
+        {
+            return self.DoAndSubmitDB(AddrForHTTP(appId, queryPreparedMethod), db, busiCall, cancellationToken);
+        }
+
+        public static Task DoAndSubmitUseDapr(this Msg self, string appId, string queryPreparedMethod, Func<BranchBarrier, Task> busiCall, CancellationToken cancellationToken = default)
+        {
+            return self.DoAndSubmit(AddrForHTTP(appId, queryPreparedMethod), busiCall, cancellationToken);
+        }
+
+        public static Task<string> CallBranchUseDapr(this Tcc self,object body, string appId, string tryMethod, string confirmMethod, string cancelMethod, CancellationToken cancellationToken = default)
+        {
+            return self.CallBranch(body, AddrForHTTP(appId, tryMethod), AddrForHTTP(appId, confirmMethod), AddrForHTTP(appId, cancelMethod), cancellationToken);
         }
     }
 }
