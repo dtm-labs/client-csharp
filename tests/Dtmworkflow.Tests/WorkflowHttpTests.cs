447cda11c19603288d4ca439f05ba2f008c24378
yxchao <yxchao127@hotmail.com>
2025-04-24 09:00:58 +0800

    Inside the 'Process' method, a check for the 'err' variable is added to make sure an error is thrown if 'err' is not null. Modify the exception assertion of the 'Rollback_Should_Be_Executed' single test to use 'Assert.ThrowsAsync' to verify whether a 'DtmFailureException' was thrown.

--- tests/Dtmworkflow.Tests/WorkflowHttpTests.cs
+++ tests/Dtmworkflow.Tests/WorkflowHttpTests.cs
@@ -208,7 +208,7 @@ namespace Dtmworkflow.Tests
             wfgt.Register(wfName, handler);
             var req = JsonSerializer.Serialize(new { userId = "1", amount = 30 });
 
-            var res = await wfgt.Execute(wfName, gid, Encoding.UTF8.GetBytes(req), true);
+            var res = await Assert.ThrowsAsync<DtmFailureException>(async () => await wfgt.Execute(wfName, gid, Encoding.UTF8.GetBytes(req), true));
 
             func.Verify(x => x.Invoke(It.IsAny<BranchBarrier>()), Times.Once);
         }
