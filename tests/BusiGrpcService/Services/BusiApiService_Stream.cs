5379b8935e05f509b761e925dd648e5b77eaeacc
徐云金YunjinXu <xuyunjin@foxmail.com>
2025-05-06 17:18:57 +0800

    refactor(BusiGrpcService): Add branchBarrier.Call demo with TransactionScope
    - commented, because without database

--- tests/BusiGrpcService/Services/BusiApiService_Stream.cs
+++ tests/BusiGrpcService/Services/BusiApiService_Stream.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using System.Text.Json;
+using System.Transactions;
 using busi;
 using DtmCommon;
 using Google.Protobuf.WellKnownTypes;
@@ -13,53 +15,63 @@ public partial class BusiApiService
         // stream try -> confirm/cancel
         await foreach (var request in requestStream.ReadAllAsync())
         {
-            BranchBarrier barrier = _barrierFactory.CreateBranchBarrier(
+            BranchBarrier branchBarrier = _barrierFactory.CreateBranchBarrier(
                 request.DtmBranchTransInfo.TransType,
                 request.DtmBranchTransInfo.Gid,
                 request.DtmBranchTransInfo.BranchId,
                 request.DtmBranchTransInfo.Op, _logger);
             _logger.LogInformation(
-                $"{nameof(StreamTransOutTcc)} gid={barrier.Gid} branch_id={barrier.BranchID} op={barrier.Op}, req={JsonSerializer.Serialize(request)}");
-            // barrier.Call(db, transaction =>
+                $"{nameof(StreamTransOutTcc)} gid={branchBarrier.Gid} branch_id={branchBarrier.BranchID} op={branchBarrier.Op}, req={JsonSerializer.Serialize(request)}");
+
+            // using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required,
+            //            new TransactionOptions()
+            //            {
+            //                IsolationLevel = IsolationLevel.ReadCommitted
+            //            },
+            //            TransactionScopeAsyncFlowOption.Enabled))
             // {
-            //
-            // });
+            //     //DbConnectionManager.GetConnection("TestDB")
+            //     await branchBarrier.Call(db: null, async () =>
+            //     {
+                    switch (request.OperateType)
+                    {
+                        case OperateType.Try:
+                        {
+                            if (string.IsNullOrWhiteSpace(request.BusiRequest.TransOutResult) || request.BusiRequest.TransOutResult.Equals("SUCCESS"))
+                            {
+                                await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Tried, waiting your confirm..." });
+                            }
+                            else if (request.BusiRequest.TransOutResult.Equals("FAILURE"))
+                            {
+                                throw new Grpc.Core.RpcException(new Status(StatusCode.Aborted, "FAILURE"));
+                            }
+                            else if (request.BusiRequest.TransOutResult.Equals("ONGOING"))
+                            {
+                                throw new Grpc.Core.RpcException(new Status(StatusCode.FailedPrecondition, "ONGOING"));
+                            }
+                            else
+                            {
+                                throw new Grpc.Core.RpcException(new Status(StatusCode.Internal, $"unknow result {request.BusiRequest.TransOutResult}"));
+                            }
 
-            switch (request.OperateType)
-            {
-                case OperateType.Try:
-                {
-                    if (string.IsNullOrWhiteSpace(request.BusiRequest.TransOutResult) || request.BusiRequest.TransOutResult.Equals("SUCCESS"))
-                    {
-                        await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Tried, waiting your confirm..." });
-                    }
-                    else if (request.BusiRequest.TransOutResult.Equals("FAILURE"))
-                    {
-                        throw new Grpc.Core.RpcException(new Status(StatusCode.Aborted, "FAILURE"));
-                    }
-                    else if (request.BusiRequest.TransOutResult.Equals("ONGOING"))
-                    {
-                        throw new Grpc.Core.RpcException(new Status(StatusCode.FailedPrecondition, "ONGOING"));
-                    }
-                    else
-                    {
-                        throw new Grpc.Core.RpcException(new Status(StatusCode.Internal, $"unknow result {request.BusiRequest.TransOutResult}"));
-                    }
-
-                    break;
-                }
-                case OperateType.Confirm:
-                {
-                    await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Confirmed" });
-                    break;
-                }
-                case OperateType.Cancel:
-                {
-                    await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Canceled" });
-                    break;
-                }
-                default:
-                    throw new ArgumentOutOfRangeException();
+                            break;
+                        }
+                        case OperateType.Confirm:
+                        {
+                            await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Confirmed" });
+                            break;
+                        }
+                        case OperateType.Cancel:
+                        {
+                            await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Canceled" });
+                            break;
+                        }
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                //     }
+                // });
+                //
+                // scope.Complete();
             }
         }
 
@@ -78,10 +90,6 @@ public partial class BusiApiService
                 request.DtmBranchTransInfo.Op, _logger);
             _logger.LogInformation(
                 $"{nameof(StreamTransOutTcc)} trans_type={barrier.TransType} gid={barrier.Gid} branch_id={barrier.BranchID} op={barrier.Op}, req={JsonSerializer.Serialize(request)}");
-            // barrier.Call(db, transaction =>
-            // {
-            //
-            // });
 
             switch (request.OperateType)
             {
