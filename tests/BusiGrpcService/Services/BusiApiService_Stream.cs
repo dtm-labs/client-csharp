ea28bcd3329a326b4255af41029c749f0e97bc1c
徐云金YunjinXu <xuyunjin@foxmail.com>
2025-05-07 10:19:22 +0800

    feature(BusiGrpcService): streamline gRPC service methods BranchBarrier.call demo

--- tests/BusiGrpcService/Services/BusiApiService_Stream.cs
+++ tests/BusiGrpcService/Services/BusiApiService_Stream.cs
@@ -5,6 +5,7 @@ using busi;
 using DtmCommon;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using MySqlConnector;
 
 namespace BusiGrpcService.Services;
 
@@ -15,67 +16,70 @@ public partial class BusiApiService
         // stream try -> confirm/cancel
         await foreach (var request in requestStream.ReadAllAsync())
         {
-            BranchBarrier branchBarrier = _barrierFactory.CreateBranchBarrier(
-                request.DtmBranchTransInfo.TransType,
-                request.DtmBranchTransInfo.Gid,
-                request.DtmBranchTransInfo.BranchId,
-                request.DtmBranchTransInfo.Op, _logger);
-            _logger.LogInformation(
-                $"{nameof(StreamTransOutTcc)} gid={branchBarrier.Gid} branch_id={branchBarrier.BranchID} op={branchBarrier.Op}, req={JsonSerializer.Serialize(request)}");
+            if (request.DtmBranchTransInfo != null)
+            {
+                BranchBarrier branchBarrier = _barrierFactory.CreateBranchBarrier(
+                    request.DtmBranchTransInfo.TransType,
+                    request.DtmBranchTransInfo.Gid,
+                    request.DtmBranchTransInfo.BranchId,
+                    request.DtmBranchTransInfo.Op, _logger);
+                _logger.LogInformation(
+                    $"{nameof(StreamTransOutTcc)} gid={branchBarrier.Gid} branch_id={branchBarrier.BranchID} op={branchBarrier.Op}, req={JsonSerializer.Serialize(request)}");
 
-            // using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required,
-            //            new TransactionOptions()
-            //            {
-            //                IsolationLevel = IsolationLevel.ReadCommitted
-            //            },
-            //            TransactionScopeAsyncFlowOption.Enabled))
-            // {
-            //     //DbConnectionManager.GetConnection("TestDB")
-            //     await branchBarrier.Call(db: null, async () =>
-            //     {
-                    switch (request.OperateType)
-                    {
-                        case OperateType.Try:
-                        {
-                            if (string.IsNullOrWhiteSpace(request.BusiRequest.TransOutResult) || request.BusiRequest.TransOutResult.Equals("SUCCESS"))
-                            {
-                                await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Tried, waiting your confirm..." });
-                            }
-                            else if (request.BusiRequest.TransOutResult.Equals("FAILURE"))
-                            {
-                                throw new Grpc.Core.RpcException(new Status(StatusCode.Aborted, "FAILURE"));
-                            }
-                            else if (request.BusiRequest.TransOutResult.Equals("ONGOING"))
-                            {
-                                throw new Grpc.Core.RpcException(new Status(StatusCode.FailedPrecondition, "ONGOING"));
-                            }
-                            else
-                            {
-                                throw new Grpc.Core.RpcException(new Status(StatusCode.Internal, $"unknow result {request.BusiRequest.TransOutResult}"));
-                            }
-
-                            break;
-                        }
-                        case OperateType.Confirm:
-                        {
-                            await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Confirmed" });
-                            break;
-                        }
-                        case OperateType.Cancel:
-                        {
-                            await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Canceled" });
-                            break;
-                        }
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                //     }
-                // });
-                //
-                // scope.Complete();
+                await using MySqlConnection conn = GetBarrierConn();
+                await branchBarrier.Call(conn, async () =>
+                {
+                    // business logic
+                    await TransOutFn(responseStream, request);
+                });
+            }
+            else
+            {
+                await TransOutFn(responseStream, request);
             }
         }
 
         _logger.LogInformation($"{nameof(StreamTransOutTcc)} completed");
+    }
+
+    private static async Task TransOutFn(IServerStreamWriter<StreamReply> responseStream, StreamRequest request)
+    {
+        switch (request.OperateType)
+        {
+            case OperateType.Try:
+            {
+                if (string.IsNullOrWhiteSpace(request.BusiRequest.TransOutResult) || request.BusiRequest.TransOutResult.Equals("SUCCESS"))
+                {
+                    await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Tried, waiting your confirm..." });
+                }
+                else if (request.BusiRequest.TransOutResult.Equals("FAILURE"))
+                {
+                    throw new Grpc.Core.RpcException(new Status(StatusCode.Aborted, "FAILURE"));
+                }
+                else if (request.BusiRequest.TransOutResult.Equals("ONGOING"))
+                {
+                    throw new Grpc.Core.RpcException(new Status(StatusCode.FailedPrecondition, "ONGOING"));
+                }
+                else
+                {
+                    throw new Grpc.Core.RpcException(new Status(StatusCode.Internal, $"unknow result {request.BusiRequest.TransOutResult}"));
+                }
+
+                break;
+            }
+            case OperateType.Confirm:
+            {
+                await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Confirmed" });
+                break;
+            }
+            case OperateType.Cancel:
+            {
+                await responseStream.WriteAsync(new StreamReply { OperateType = request.OperateType, Message = "Canceled" });
+                break;
+            }
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     public override async Task StreamTransInTcc(IAsyncStreamReader<StreamRequest> requestStream, IServerStreamWriter<StreamReply> responseStream, ServerCallContext context)
@@ -83,64 +87,81 @@ public partial class BusiApiService
         // stream try -> confirm/cancel
         await foreach (var request in requestStream.ReadAllAsync())
         {
-            BranchBarrier barrier = _barrierFactory.CreateBranchBarrier(
-                request.DtmBranchTransInfo.TransType,
-                request.DtmBranchTransInfo.Gid,
-                request.DtmBranchTransInfo.BranchId,
-                request.DtmBranchTransInfo.Op, _logger);
-            _logger.LogInformation(
-                $"{nameof(StreamTransOutTcc)} trans_type={barrier.TransType} gid={barrier.Gid} branch_id={barrier.BranchID} op={barrier.Op}, req={JsonSerializer.Serialize(request)}");
-
-            switch (request.OperateType)
+            if (request.DtmBranchTransInfo != null)
             {
-                case OperateType.Try:
-                {
-                    if (string.IsNullOrWhiteSpace(request.BusiRequest.TransInResult) || request.BusiRequest.TransInResult.Equals("SUCCESS"))
-                    {
-                        await responseStream.WriteAsync(new StreamReply
-                        {
-                            OperateType = request.OperateType,
-                            Message = "Tried, waiting your confirm..."
-                        });
-                    }
-                    else if (request.BusiRequest.TransInResult.Equals("FAILURE"))
-                    {
-                        throw new Grpc.Core.RpcException(new Status(StatusCode.Aborted, "FAILURE"));
-                    }
-                    else if (request.BusiRequest.TransInResult.Equals("ONGOING"))
-                    {
-                        throw new Grpc.Core.RpcException(new Status(StatusCode.FailedPrecondition, "ONGOING"));
-                    }
-                    else
-                    {
-                        throw new Grpc.Core.RpcException(new Status(StatusCode.Internal, $"unknow result {request.BusiRequest.TransInResult}"));
-                    }
+                BranchBarrier branchBarrier = _barrierFactory.CreateBranchBarrier(
+                    request.DtmBranchTransInfo.TransType,
+                    request.DtmBranchTransInfo.Gid,
+                    request.DtmBranchTransInfo.BranchId,
+                    request.DtmBranchTransInfo.Op, _logger);
+                _logger.LogInformation(
+                    $"{nameof(StreamTransInTcc)} gid={branchBarrier.Gid} branch_id={branchBarrier.BranchID} op={branchBarrier.Op}, req={JsonSerializer.Serialize(request)}");
 
-                    break;
-                }
-                case OperateType.Confirm:
+                await using MySqlConnection conn = GetBarrierConn();
+                await branchBarrier.Call(conn, async () =>
                 {
-                    await responseStream.WriteAsync(new StreamReply
-                    {
-                        OperateType = request.OperateType,
-                        Message = "Confirmed"
-                    });
-                    break;
-                }
-                case OperateType.Cancel:
-                {
-                    await responseStream.WriteAsync(new StreamReply
-                    {
-                        OperateType = request.OperateType,
-                        Message = "Canceled"
-                    });
-                    break;
-                }
-                default:
-                    throw new ArgumentOutOfRangeException();
+                    // business logic
+                    await TransInFn(responseStream, request);
+                });
+            }
+            else
+            {
+                await TransInFn(responseStream, request);
             }
         }
 
         _logger.LogInformation($"{nameof(StreamTransInTcc)} completed");
+    }
+
+    private static async Task TransInFn(IServerStreamWriter<StreamReply> responseStream, StreamRequest request)
+    {
+        switch (request.OperateType)
+        {
+            case OperateType.Try:
+            {
+                if (string.IsNullOrWhiteSpace(request.BusiRequest.TransInResult) || request.BusiRequest.TransInResult.Equals("SUCCESS"))
+                {
+                    await responseStream.WriteAsync(new StreamReply
+                    {
+                        OperateType = request.OperateType,
+                        Message = "Tried, waiting your confirm..."
+                    });
+                }
+                else if (request.BusiRequest.TransInResult.Equals("FAILURE"))
+                {
+                    throw new Grpc.Core.RpcException(new Status(StatusCode.Aborted, "FAILURE"));
+                }
+                else if (request.BusiRequest.TransInResult.Equals("ONGOING"))
+                {
+                    throw new Grpc.Core.RpcException(new Status(StatusCode.FailedPrecondition, "ONGOING"));
+                }
+                else
+                {
+                    throw new Grpc.Core.RpcException(new Status(StatusCode.Internal, $"unknow result {request.BusiRequest.TransInResult}"));
+                }
+
+                break;
+            }
+            case OperateType.Confirm:
+            {
+                await responseStream.WriteAsync(new StreamReply
+                {
+                    OperateType = request.OperateType,
+                    Message = "Confirmed"
+                });
+                break;
+            }
+            case OperateType.Cancel:
+            {
+                await responseStream.WriteAsync(new StreamReply
+                {
+                    OperateType = request.OperateType,
+                    Message = "Canceled"
+                });
+                break;
+            }
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
